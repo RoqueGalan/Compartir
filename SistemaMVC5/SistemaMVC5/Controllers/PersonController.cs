4cd8bac9f93a9c3ad07a77c90d228f0eae1912ce
roqueGalan <imusicreggae@gmail.com>
2015-04-13 19:02:48 -0500

    ejemplo mvc dias

---
+++ SistemaMVC5/SistemaMVC5/Controllers/PersonController.cs
@@ -0,0 +1,63 @@
+﻿using Angela.Core;
+using SistemaMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaMVC5.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static ICollection<Person> _people;
+
+        static PersonController()
+        {
+            _people = Angie.Configure<Person>()
+                .Fill(p => p.BirthDate)
+                .AsPastDate()
+                .Fill(p => p.LikesMusic)
+                .WithRandom(new List<bool>() { true, true, true, false, false })
+                .Fill(p => p.Skills, () => new List<string>() { "Math", "Science", "History" })
+                .MakeList<Person>(20);
+        }
+
+        public ActionResult SearchPeople(string searchText)
+        {
+            var term = searchText.ToLower();
+            var result = _people
+                .Where(p =>
+                    p.FirstName.ToLower().Contains(term) ||
+                    p.LastName.ToLower().Contains(term)
+                );
+
+            return PartialView("_SearchPeople", result);
+        }
+
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(_people);
+        }
+
+        public ActionResult Create()
+        {
+            var person = new Person();
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _people.Add(person);
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+    }
+}
---
+++ SistemaMVC5/SistemaMVC5/Controllers/SimpleController.cs
@@ -0,0 +1,34 @@
+﻿using SistemaMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaMVC5.Controllers
+{
+    public class SimpleController : Controller
+    {
+        // GET: Simple
+        public ActionResult Index()
+        {
+            var person = new Person
+            {
+                FirstName = "Roque",
+                LastName = "Galan",
+                BirthDate = new DateTime(1989, 2, 7),
+                LikesMusic = true,
+                EmailAddress = "roque.ggalan@gmail.com",
+                Skills = new List<string>() { "Math", "Science", "History" }
+            };
+
+            return View(person);
+        }
+
+        public ActionResult Create()
+        {
+            var person = new Person();
+            return View(person);
+        }
+    }
+}
